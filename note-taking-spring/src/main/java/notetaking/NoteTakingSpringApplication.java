1ad728f15902b1708300de2542bd549edfb86b09
matheusrebola <m.cavalcantir@outlook.com>
2024-07-17 23:31:40 -0300

    feat: criação do projeto spring

---
+++ note-taking-spring/src/main/java/notetaking/NoteTakingSpringApplication.java
@@ -0,0 +1,13 @@
+package notetaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteTakingSpringApplication.class, args);
+	}
+
+}
---
+++ note-taking-spring/src/test/java/notetaking/NoteTakingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package notetaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
